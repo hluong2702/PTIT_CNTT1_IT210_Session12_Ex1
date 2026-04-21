9fb34fdfb807f954e0b7356ec88ce64b82b1f2a2
hoangluong <hoangluong@bibabibo-2.local>
2026-04-22 03:42:07 +0700

    done

---
+++ src/main/java/org/example/ptit_cntt1_it210_session12_ex1/PtitCntt1It210Session12Ex1Application.java
@@ -0,0 +1,14 @@
+package org.example.ptit_cntt1_it210_session12_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitCntt1It210Session12Ex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtitCntt1It210Session12Ex1Application.class, args);
+        System.out.println("Hospital Web Service is ready on Embedded Tomcat!");
+    }
+
+}
---
+++ src/test/java/org/example/ptit_cntt1_it210_session12_ex1/PtitCntt1It210Session12Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ptit_cntt1_it210_session12_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtitCntt1It210Session12Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
